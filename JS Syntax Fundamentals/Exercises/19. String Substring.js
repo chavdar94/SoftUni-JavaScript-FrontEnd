c3e919530a5404c4eca31d0620f5e82b49b46ed6
Chavdar <chokobg@gmail.com>
2023-02-22 22:20:00 +0200

    exercises init

--- JS Syntax Fundamentals/Exercises/19. String Substring.js
+++ JS Syntax Fundamentals/Exercises/19. String Substring.js
@@ -1,17 +1,11 @@
 function findSubstring(searchedWord, text) {
-  let condition = false;
-
   for (let word of text.split(" ")) {
     if (word.toLowerCase() === searchedWord.toLowerCase()) {
       console.log(searchedWord);
-      condition = true;
       return;
     }
   }
-
-  if (condition === false) {
-    console.log(`${searchedWord} not found!`);
-  }
+  console.log(`${searchedWord} not found!`);
 }
 
 // findSubstring("javascript", "JavaScript is the best programming language");
--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-let array = [1, 2, 3, 4, 5, 6];
-let newArray = []
-for (let i = 0; i <= 3; i++) {
-  newArray.push(array.pop());
+function solve(word, text) {
+  text = text.toLowerCase();
+  if (text.includes(word.toLowerCase())) {
+    console.log(word);
+    return;
+  } else {
+    console.log(`${word} not found!`);
+  }
 }
-
-console.log(array);
